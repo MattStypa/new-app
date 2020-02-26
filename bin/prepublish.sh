e9743d8a87901c0871e8ada2ddade6ca6127319f
mattstypa <matt@mattstypa.com>
2020-02-25 23:48:01 -0600

    More tweaks to precommit script

--- bin/prepublish.sh
+++ bin/prepublish.sh
@@ -1,14 +1,21 @@
+echo 'Have you updated these things:'
+read -p '- changelog'
+read -p '- readme.md documentation link'
+read -p '- package.js version'
+echo
+
 if [[ $(git status -s) ]]; then
   echo 'There are uncommitted changes!'
   echo
   exit 1
 fi
 
-echo 'Have you updated these things:'
-read -p '- changelog'
-read -p '- readme.md documentation link'
-read -p '- package.js version'
-echo
+if [[ $(git cherry -v) ]]; then
+  echo 'There are upushed commits!'
+  echo
+  exit 1
+fi
+
 read -p 'Have you published the release in GitHub?'
 
 npm run build
